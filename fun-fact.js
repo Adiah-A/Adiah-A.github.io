111802740c99e5d9c8ec356f26c61fb2bd7811f4
Adiah Amadou <amadouadiah@gmail.com>
2023-10-30 23:45:31 -0400

    Changed the list form fun facts to favorite songs

--- fun-fact.js
+++ fun-fact.js
@@ -1,7 +1,24 @@
 const funFacts = [
-    "Je parle couramment Français!",
-    "Hot Take Alert: I think Mac and Cheese is overrated (email to argue)"
-    //"I'm a coffee connoisseur with over 20 coffee mugs."
+    "Big7 - Burna boy",
+    "Siamois - Fally Ipupa",
+    "AfroTrap, Pt. 7 (La Puissance) - MHD",
+    "It's Plenty - Burna boy",
+    "City Boys - Burna boy",
+    "A Kele Nta - MHD",
+    "Tested, Approved & Trusted - Burna boy",
+    "Eloko Oyo - Fally Ipupa",
+    "For My Hand (feat. Ed Sheeran) - Burna boy",
+    "AfroTrap, Pt. 3 (Champions League) - MHD",
+    "Alone - Burna boy",
+    "Last Last - Burna boy",
+    "Location - Burna boy",
+    "Tout le Monde - Fally Ipupa",
+    "Ye - Burna boy",
+    "Ipseite - Damso",
+    "Copines - Aya Nakamura",
+    "La dot - Aya Nakamura",
+    "Feu de bois - Damso",
+    "AfroTrap, Pt. 5 (Ngatie Abedi) - MHD"
   ];
   
   // Function to display a random fun fact
